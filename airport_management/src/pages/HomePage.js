81a778be4fdf1da9da93fc48e49418162503fb05
matusalik <mateuszsalik@gmail.com>
2025-04-11 11:51:10 +0200

    Finished the login page

--- airport_management/src/pages/HomePage.js
+++ airport_management/src/pages/HomePage.js
@@ -1,8 +1,10 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import './HomePage.css'
 function HomePage(){
     const [loginValue, setLoginValue] = useState("");
     const [passwordValue, setPasswordValue] = useState("");
+    const [errorLabel, setErrorLabel] = useState("");
     const handleLoginChange = (e) => {
         setLoginValue(e.target.value);
     };
@@ -10,7 +12,23 @@ function HomePage(){
         setPasswordValue(e.target.value);
     };
     const handleLogin = () =>{
+        axios.get('http://localhost:8080/users/findAllUsers')
+        .then((response) => {
+            const foundUser = response.data.find(
+                (u) => u.userLogin === loginValue.trim() && u.userPassword === passwordValue.trim()
+            );
 
+            if (foundUser) {
+                console.log("SUCCESS");
+                setErrorLabel("");
+            } 
+            else {
+                setErrorLabel("Invalid login or password!");
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching users:", error);
+        });
     }
     return(
         <div className='outerHome'>
@@ -24,14 +42,17 @@ function HomePage(){
                     onChange={handleLoginChange}
                 /><br/>        
                 <input className='homePageInput'
-                    type="text"
+                    type="password"
                     placeholder="Password"
                     value={passwordValue}
                     onChange={handlePasswordChange}
                 /><br/>
                 <button className='homePageButton' onClick={handleLogin}>
                     Log in
-                </button>
+                </button><br/>
+                <label className='errorLabel'>
+                    {errorLabel}
+                </label>
             </div>
         </div>
     );
